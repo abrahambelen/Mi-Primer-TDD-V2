7900e1f7f994d3882d3fa76deb6ea5ed2105d6cf
Abraham <abraham.belen.duran@gmail.com>
2023-03-30 14:17:30 +0200

    Refactorización de los métodos

--- src/Coche.java
+++ src/Coche.java
@@ -4,13 +4,13 @@ public class Coche {
 	public int velocidad;
 
 
-	public void acelerar(int aceleracion) {
+	public void acelerar_Abraham_Belen_Duran(int aceleracion) {
 		velocidad = velocidad + aceleracion;
 		
 	}
 
 
-	public void decelerar(int deceleracion) {
+	public void decelerar_Abraham_Belen_Duran(int deceleracion) {
 		velocidad = velocidad - deceleracion;
 		if (velocidad <0) {
 			velocidad = 0;
