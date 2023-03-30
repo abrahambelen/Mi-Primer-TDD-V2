823d3617509441688ea091d75bbcab655816f066
Abraham <abraham.belen.duran@gmail.com>
2023-03-30 12:44:16 +0200

    Segundo test creado

--- src/tests.java
+++ src/tests.java
@@ -1,12 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class tests {
 
 	@Test
-	void test_crear_coche() {
+	void test_al_crear_un_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 
 }
